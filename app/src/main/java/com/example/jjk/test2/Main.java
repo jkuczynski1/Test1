9553b62df256f1108a302e72e1e820f35202bc6f
jkuczynski1 <jkuczynski1@luc.edu>
2017-01-18 19:48:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/jjk/test2/Main.java
@@ -0,0 +1,12 @@
+package com.example.jjk.test2;
+
+/**
+ * Created by jjk on 1/18/2017.
+ */
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi world.");
+    }
+}
